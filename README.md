8210eeb7df91eb5d2f1e707448c9d282ea89836f
Eli Silk <esilk@me.com>
2024-09-30 09:34:57 -0400

    Initial solution.

---
+++ README.md
@@ -0,0 +1,96 @@
+# Frontend Mentor - Password generator app solution
+
+This is a solution to the [Password generator app challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/password-generator-app-Mr8CLycqjh). Frontend Mentor challenges help you improve your coding skills by building realistic projects.
+
+## Table of contents
+
+- [Overview](#overview)
+  - [The challenge](#the-challenge)
+  - [Screenshot](#screenshot)
+  - [Links](#links)
+- [My process](#my-process)
+  - [Built with](#built-with)
+  - [What I learned](#what-i-learned)
+  - [Continued development](#continued-development)
+  - [Useful resources](#useful-resources)
+- [Author](#author)
+
+## Overview
+
+### The challenge
+
+Users should be able to:
+
+- Generate a password based on the selected inclusion options
+- Copy the generated password to the computer's clipboard
+- See a strength rating for their generated password
+- View the optimal layout for the interface depending on their device's screen size
+- See hover and focus states for all interactive elements on the page
+
+### Screenshot
+
+|               Mobile designed at 375px:                |               Desktop designed at 1440px:               |
+| :----------------------------------------------------: | :-----------------------------------------------------: |
+|   ![](./screenshots/screenshot-mobile-completed.png)   |   ![](./screenshots/screenshot-desktop-completed.png)   |
+|                Mobile (active states):                 |                Desktop (active states):                 |
+| ![](./screenshots/screenshot-mobile-active-states.png) | ![](./screenshots/screenshot-desktop-active-states.png) |
+
+### Links
+
+- Solution URL: [https://github.com/elisilk/password-generator-app](https://github.com/elisilk/password-generator-app)
+- Live Site URL: [https://elisilk.github.io/password-generator-app/](https://elisilk.github.io/password-generator-app/)
+
+## My process
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS custom properties
+- Flexbox
+- CSS Grid
+- Mobile-first workflow
+- Fluid typography and spacing
+
+### What I learned
+
+- working with SVGs
+- styling checkboxes
+- styling ranges/sliders
+
+- [SVG hover](https://css-tricks.com/change-color-of-svg-on-hover/) and the [different ways to use SVG in HTML markup](https://css-tricks.com/using-svg/)
+
+### Continued development
+
+Hmm 🤔 ...
+
+Specific areas that the solution should be improved:
+
+- In defiance of the design's "Empty" state, choose to set up initial state (in HTML and in JS) with some amount of checkboxes checked and the slider at some reasonable start value (e.g., 12)
+- Slight vertical shift in the `.strength-eval` element when unhiding one of the strength labels
+- Get copy button functional
+- Get copy button icon to resize appropriately
+- Add word "Copied" after copy success (and take away after some set period of time?)
+- Figure out where extra vertical spacing in the copy button is coming from and remove it
+- Error checking
+  - character length slider value > 0
+  - at least one of the checkboxes checked
+  - (make the error states show up somewhere?)
+- Get checkbox list functional
+- Decide what special characters are allowed
+- Check over spacing and font sizes
+- Why does the generate button stay active/focused after a click? What should happen?
+- Use lambdatest to test on different browsers
+
+More general ideas I want to consider:
+
+### Useful resources
+
+- [MDN Web Docs for CSS](https://developer.mozilla.org/en-US/docs/Web/CSS) - Went here a lot to reference the different CSS properties and the shorthands, and all the great explanations about best practices.
+- [MDN Guides](https://developer.mozilla.org/en-US/docs/Learn) - So many resources and nice explanations, including stuff [tutorials specific to Javascript](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide).
+- [Every Layout](https://every-layout.dev/) - My go to source for thinking about layouts now.
+- [The Clamp Calculator](https://royalfig.github.io/fluid-typography-calculator/) - Used for all of fluid typography and fluid spacing calculations.
+
+## Author
+
+- Website - [Eli Silk](https://github.com/elisilk)
+- Frontend Mentor - [@elisilk](https://www.frontendmentor.io/profile/elisilk)
---
+++ script.js
@@ -0,0 +1,205 @@
+// ----------------------------------------
+// HTML elements to save
+// ----------------------------------------
+
+// The form overall
+const passwordGeneratorForm = document.getElementById(
+  "password-generator-form"
+);
+
+// Key elements
+const outputTextElement = document.getElementById("output-text");
+const outputCopyIcon = document.getElementById("output-copy-icon");
+const outputCopySuccess = document.getElementById("output-copy-success");
+
+const generateButtonElement = document.getElementById("generate-button");
+
+const characterLengthOutput = document.getElementById(
+  "character-length-output"
+);
+const characterLengthSlider = document.getElementById(
+  "character-length-slider"
+);
+
+// Key groups of elements
+const strengthEvalLevels = document.querySelectorAll(".strength-eval__level");
+
+// ----------------------------------------
+// Helper Functions
+// ----------------------------------------
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
+
+// ----------------------------------------
+// Functions
+// ----------------------------------------
+
+const generateASCIICharacters = (start = 33, end = 126) => {
+  let result = "";
+
+  for (let i = start; i <= end; i++) {
+    result += String.fromCharCode(i);
+  }
+
+  return result;
+};
+
+const generateNewPassword = (length) => {
+  //return "test";
+
+  if (length <= 0) return false;
+
+  const charactersUppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  const charactersLowercase = "abcdefghijklmnopqrstuvwxyz";
+  const charactersNumbers = "0123456789";
+  const charactersSymbols = `~!@#$%^&*()_-+={[}]|:;<,>.?/`;
+
+  const charactersAllowed =
+    charactersUppercase +
+    charactersLowercase +
+    charactersNumbers +
+    charactersSymbols;
+
+  const charactersLength = charactersAllowed.length;
+
+  let result = "";
+  let counter = 0;
+
+  while (counter < length) {
+    result += charactersAllowed.charAt(
+      Math.floor(Math.random() * charactersLength)
+    );
+    counter += 1;
+  }
+
+  return result;
+};
+
+// validations for this specific form
+const strengthLabels = {
+  0: "too-weak",
+  1: "weak",
+  2: "medium",
+  3: "strong",
+};
+
+const evaluatePasswordStrength = (password) => {
+  // 0 - Too Weak!
+  // 1 - Weak
+  // 2 - Medium
+  // 3 - Strong
+
+  return getRandomInt(4);
+};
+
+const showStrengthEval = (strength, labels) => {
+  strengthEvalLevels.forEach((level) => {
+    //console.log(level.classList);
+    //console.log(level.classList.contains("strength-eval__level--hide"));
+
+    if (!level.classList.contains("strength-eval__level--hide")) {
+      level.classList.add("strength-eval__level--hide");
+    }
+
+    //console.log("strength-eval__level--" + labels[strength]);
+    if (level.classList.contains("strength-eval__level--" + labels[strength])) {
+      level.classList.remove("strength-eval__level--hide");
+    }
+  });
+
+  //console.log("Strength: " + labels[strength]);
+};
+
+const copyPasswordToClipboard = (password) => {
+  if (password == "") {
+    console.log("Copying: (nothing to copy)");
+    return false;
+  }
+
+  // Check for permissions
+  navigator.permissions.query({ name: "clipboard-write" }).then((result) => {
+    if (!(result.state == "granted" || result.state == "prompt")) {
+      console.error("Copying: Do not have permissions");
+      return false;
+    }
+  });
+
+  // Copy to cliphboard
+  navigator.clipboard.writeText(password).then(
+    () => {
+      console.log("Copied password: " + password);
+      /* Resolved - text copied to clipboard successfully */
+      return true;
+    },
+    () => {
+      console.error("Copy: Failed to copy");
+      return false;
+      /* Rejected - text failed to copy to the clipboard */
+    }
+  );
+  //document.execCommand("copy");
+};
+
+const clearFocus = () => {
+  if (document.activeElement && document.activeElement !== document.body) {
+    document.activeElement.blur();
+  }
+};
+
+// ----------------------------------------
+// Event Listener Functions
+// ----------------------------------------
+
+const handleCopy = (e) => {
+  //console.log("copy button: " + e.target.value);
+  let currrentPassword = outputTextElement.value;
+
+  if (copyPasswordToClipboard(currrentPassword)) {
+    // Show success state
+    outputCopySuccess.style.setProperty("visibility", "visible");
+  }
+
+  setTimeout(() => {
+    // Clear success state
+    outputCopySuccess.style.setProperty("visibility", "hidden");
+    // Clear input focus
+    clearFocus();
+  }, 6000);
+};
+
+const handleSlider = (e) => {
+  //console.log("slider: " + e.target.value);
+  characterLengthOutput.textContent = e.target.value;
+};
+
+const handleSubmit = (e) => {
+  e.preventDefault();
+
+  //console.log("submitted");
+  //console.log(generateASCIICharacters(33, 126));
+
+  let newPassword = generateNewPassword(Number(characterLengthSlider.value));
+  if (newPassword) {
+    outputTextElement.value = newPassword;
+    outputTextElement.disabled = false;
+  }
+
+  showStrengthEval(evaluatePasswordStrength(newPassword), strengthLabels);
+
+  // The input will lose focus after 3 seconds
+  setTimeout(() => {
+    clearFocus();
+  }, 3000);
+};
+
+// ----------------------------------------
+// Add event listeners to all input elements in the form
+// ----------------------------------------
+
+characterLengthSlider.addEventListener("input", handleSlider);
+outputCopyIcon.addEventListener("click", handleCopy);
+
+generateButtonElement.addEventListener("click", handleSubmit);
+passwordGeneratorForm.addEventListener("submit", handleSubmit);
